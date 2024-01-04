c33826e844d95f4269bb7c3b8e7a504ee1fbc2ff
Your Name <nguyphuquy1@gmail.com>
2024-01-05 02:22:26 +0700

    first commit

---
+++ prisma/category.ts
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: "Laptop",
+        codeName: "laptop"
+    },
+    {
+        title: "PC & phụ kiện",
+        codeName: "PC"
+    },
+    {
+        title: "Điện thoại & phụ kiện",
+        codeName: "phone"
+    },
+    {
+        title: "Thiết bị âm thanh",
+        codeName: "audio"
+    },
+    {
+        title: "Phụ kiện chung",
+        codeName: "devide"
+    }
+]
